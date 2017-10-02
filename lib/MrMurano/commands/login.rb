697c9c82cc5c798bfb44fc724a0d788e75cdb14a
landonb <landonb@users.noreply.github.com>
2017-10-02 09:43:11 -0500

    Fix login/logout command issues [in progress].

--- lib/MrMurano/commands/login.rb
+++ lib/MrMurano/commands/login.rb
@@ -1,4 +1,4 @@
-# Last Modified: 2017.09.21 /coding: utf-8
+# Last Modified: 2017.10.02 /coding: utf-8
 # frozen_string_literal: true
 
 # Copyright © 2016-2017 Exosite LLC.
@@ -10,7 +10,7 @@ require 'MrMurano/Config'
 require 'MrMurano/ReCommander'
 
 command 'login' do |c|
-  c.syntax = %(murano login)
+  c.syntax = %(murano login [--options] [<username>])
   c.summary = %(Log into Murano)
   c.description = %(
 Log into Murano.
@@ -18,14 +18,46 @@ Log into Murano.
 If you are having trouble logging in, try deleting the saved password first:
 
   murano password delete <username>
+
+You might also need to unset the password environment variable:
+
+  MURANO_PASSWORD=
+
+You can also try the logout command:
+
+  murano logout
   ).strip
+
+  c.example %(
+    Login interactively. You'll be asked for your username (email) and password.
+  ).strip, %(murano login)
+
+  c.example %(
+    Or, specify the account to use to logon.
+  ).strip, %(murano login <username>)
+
+  c.example %(
+    If you want to include the password, use an environment variable.
+  ).strip, %(MURANO_PASSWORD=xyz murano login <username>)
+
+  c.example %(
+    Developers can specify a different host than might be configured globally.
+  ).strip, %(murano login <username> --host <address>)
+
   c.option '--show-token', %(Shows the API token)
+  c.option '--host HOST', %(Same as `murano login -c net.host="a.b.c"`)
+
   c.project_not_required = true
   c.prompt_if_logged_off = true
 
 # FIXME: Clean UP:
   c.action do |args, options|
-    c.verify_arg_count!(args)
+    c.verify_arg_count!(args, 1)
+    $cfg['user.name'] = args[0] unless args[0].nil?
+    $cfg.set_net_host(options.host, :internal) unless options.host.nil?
+    MrMurano::Verbose.verbose %(
+      Checking #{$cfg['user.name']} on #{$cfg['net.protocol']}://#{$cfg['net.host']}
+    ).strip
     tok = MrMurano::Account.instance.token
     say tok if options.show_token
     # Save user.name and net.protocol/net.host to project file if successful.
@@ -40,6 +72,13 @@ If you are having trouble logging in, try deleting the saved password first:
 # and what about, say, murano solution create in empty directory,
 #   i think that works: we would have to maybe save to :project file there, too...
 #
+    unless tok.to_s.empty?
+      [
+        'user.name',
+        'net.protocol',
+        'net.host',
+      ].each { |key| $cfg.set(key, $cfg[key], :project) }
+    end
   end
 end
 
@@ -70,11 +109,19 @@ Essentially, this command is the same as:
 
   murano password delete <username>
   murano password delete <username>/twofactor
-  murano config --unset --user user.name
+
+Include the --user option to also clear the username, e.g.,
+
+  murano config --unset user.name --user
+  murano config --unset user.name --project
   ).strip
+
   c.project_not_required = true
 
-  c.option '--token', 'Remove just the two-factor token'
+  c.option '--token', 'Remove only the two-factor token'
+# FIXME: What if user specified another config?
+#   --configfile :specified / MURANO_CONFIGFILE :env / and :project and :user
+  c.option '--username', 'Also remove the user.name config values'
 
   c.action do |args, options|
     c.verify_arg_count!(args)
